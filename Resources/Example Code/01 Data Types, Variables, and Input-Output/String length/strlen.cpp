df6bc15ccf73fd289ace445cc2ec112985daa3a5
Rachel J. Morris <racheljmorris@gmail.com>
2017-06-07 19:23:12 -0500

    Create strlen.cpp

---
+++ Resources/Example Code/01 Data Types, Variables, and Input-Output/String length/strlen.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>		// Don't forget this!
+using namespace std;
+
+int main()
+{
+	string text;
+
+	cout << "Enter some text: ";
+	cin >> text;
+
+	int length = text.size();
+
+	cout << "Length is " << length << endl;
+
+
+	while (true);
+
+	return 0;
+}
