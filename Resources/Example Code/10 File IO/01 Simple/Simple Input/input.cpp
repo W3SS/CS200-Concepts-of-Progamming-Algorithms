de5793d6e7fb6c8ea07eadf68ab0da4dcf9d8c83
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2017-06-12 11:30:45 -0500

    Update example code

--- Resources/Example Code/10 File IO/01 Simple/Simple Input/input.cpp
+++ Resources/Example Code/10 File IO/01 Simple/Simple Input/input.cpp
@@ -7,16 +7,16 @@ int main()
     string word;
     ifstream infile( "input.txt" );
 
-    string words[3];
-    infile >> words[0];
-    infile >> words[1];
-    infile >> words[2];
+    string word1, word2, word3;
+    infile >> words1;
+    infile >> words2;
+    infile >> words3;
 
     infile.close();
 
-    cout << words[0] << endl
-        << words[1] << endl
-        << words[2] << endl;
+    cout << words1 << endl
+        << words2 << endl
+        << words3 << endl;
 
     return 0;
 }
